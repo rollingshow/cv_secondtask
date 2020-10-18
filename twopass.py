423f201627cf7c481096b0c3d9dce1f6c57f9048
Valeria <46083919+rollingshow@users.noreply.github.com>
2020-10-18 17:46:50 +0800

    Update twopass.py

--- twopass.py
+++ twopass.py
@@ -1,62 +1,107 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def negate(B):
+    array = B.copy()
+    array[np.where(array == 1)] = -1
+    return array
 
-def recursion(B, i, j, l):
+def check(B, y, x):
+    if not 0 <= x < B.shape[0]:
+        return False
+    if not 0 <= y < B.shape[1]:
+        return False
+    if B[y, x] != 0:
+        return True
+    return False
 
-    B[i, j] = l
-    if (B[i + 1, j] == 255):
-        recursion(B, i + 1, j, l)
-    if (B[i - 1, j] == 255):
-        recursion(B, i - 1, j, l)
-    if (B[i, j + 1] == 255):
-        recursion(B, i, j + 1, l)
-    if (B[i, j - 1] == 255):
-        recursion(B, i, j - 1, l)
+def neighbors2(B, y, x):
+    left = y, x - 1
+    top = y - 1, x
+    if not check(B, *left):
+        left = None
+    if not check(B, *top):
+        top = None
+    return left, top
 
-def recursion_labeling(B):
-    labels = np.copy(B)  # Метки
+def exists(neighbors):
+    return not all([n is None for n in neighbors])
 
+def find(label, linked):
+    j = label
+    while linked[j] != 0:
+        j = linked[j]
+    return j
+
+def union(label1, label2, linked):
+    j = find(label1, linked)
+    k = find(label2, linked)
+    if j != k:
+        linked[k] = j
+
+
+def three_pass_labeling(B):
+    linked = np.zeros(len(B), dtype="uint")
+    labels = np.zeros_like(B)
     label = 1
-    labels[labels != 0] = 255
+
     for row in range(B.shape[0]):
         for col in range(B.shape[1]):
-            if labels[row, col] == 255:
-                recursion(labels, row, col, label)
-                label += 1
-
+            if B[row, col] != 0:
+                n = neighbors2(B, row, col)
+                if not exists(n):
+                    m = label
+                    label += 1
+                else:
+                    lbs = [labels[i] for i in n if i is not None]
+                    m = min(lbs)
+                labels[row, col] = m
+                for i in n:
+                    if i is not None:
+                        lb = labels[i]
+                        if lb != m:
+                            union(m, lb, linked)
+    for row in range(B.shape[0]):
+        for col in range(B.shape[1]):
+            if B[row, col] != 0:
+                new_label = find(labels[row, col], linked)
+                if new_label != labels[row, col]:
+                    labels[row, col] = new_label
+                    
+    for new, old in enumerate(list(set(labels.ravel()))[1:]):
+        labels[labels == old] = new+1
+                                            
     return labels
+
 
 
 if __name__ == "__main__":
     B = np.zeros((20, 20), dtype='int32')
-
+    
     B[1:-1, -2] = 1
-
+    
     B[1, 1:5] = 1
     B[1, 7:12] = 1
     B[2, 1:3] = 1
     B[2, 6:8] = 1
     B[3:4, 1:7] = 1
-
+    
     B[7:11, 11] = 1
     B[7:11, 14] = 1
     B[10:15, 10:15] = 1
-
+    
     B[5:10, 5] = 1
     B[5:10, 6] = 1
 
-    LB = recursion_labeling(B)
-
-    print("Labels - ", list(set(LB.ravel()))[1:])
-
+    LB = three_pass_labeling(B)
+    
     plt.figure(figsize=(12, 5))
     plt.subplot(121)
-    plt.imshow(B)
+    plt.imshow(B, cmap="hot")
     plt.colorbar(ticks=range(int(2)))
     plt.axis("off")
     plt.subplot(122)
-    plt.imshow(LB)
+    plt.imshow(LB.astype("uint8"), cmap="hot")
     plt.colorbar()
     plt.axis("off")
     plt.tight_layout()
